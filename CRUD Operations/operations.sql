4418877ea57e17a3fea108fe668426a56da0a138
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-03 15:48:34 -0500

    update and delete operation

--- CRUD Operations/operations.sql
+++ CRUD Operations/operations.sql
@@ -11,3 +11,9 @@ SELECT COUNT(Name) FROM country WHERE population > 10000;
 
 INSERT INTO customer (name, address, city, state, zip)
 VALUES ('Avinash Shanker', '14000 Rennaisance Ct', 'Austin', 'TX', '78728');
+
+SELECT * FROM customer;
+
+UPDATE customer set state = 'AP' WHERE state = 'AZ';
+
+DELETE FROM customer where id = 5;
