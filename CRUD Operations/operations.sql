98b87c2de0fdc24487ac6084b51bda5a0d0e9252
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-03 15:39:04 -0500

    insert

--- CRUD Operations/operations.sql
+++ CRUD Operations/operations.sql
@@ -6,3 +6,8 @@ SELECT * FROM country WHERE continent = "Europe"
 ORDER BY Name
 LIMIT 5
 OFFSET 10;
+
+SELECT COUNT(Name) FROM country WHERE population > 10000;
+
+INSERT INTO customer (name, address, city, state, zip)
+VALUES ('Avinash Shanker', '14000 Rennaisance Ct', 'Austin', 'TX', '78728');
